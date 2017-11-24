7df0a3638776b6ce83d3322483b81fbb1da4bf90
Akansha Jain <akansha.da1@iiitmk.ac.in>
2017-11-24 18:15:31 +0530

    Add files via upload

---
+++ Que3_code_I.py
@@ -0,0 +1,15 @@
+import wikipedia
+import nltk
+#wikipedia.set_lang('en')
+#page = wikipedia.page('Wikipedia')
+#print(page.content)
+
+url='https://en.wikipedia.org/wiki/Sachin_Tendulkar'
+title=url.split("/")[-1]
+wiki_obj= wikipedia.page(title)
+text=wiki_obj.content
+
+for sent in nltk.sent_tokenize(text):
+    for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
+        if hasattr(chunk, 'label'):
+            print(' '.join(c[0] for c in chunk),':',chunk.label())
