4ad51e6e0b6426677797b24d1e4c6be334367ca2
Akansha Jain <akansha.da1@iiitmk.ac.in>
2017-11-24 18:18:24 +0530

    NER tagging on wiki's page content

--- wikipedia_url_content_extraction.py
+++ wikipedia_url_content_extraction.py
@@ -9,6 +9,10 @@ title=url.split("/")[-1]
 wiki_obj= wikipedia.page(title)
 text=wiki_obj.content
 
+#All text content from the page
+print(text)
+
+#To display all NER tags from the wikipedia's page content.
 for sent in nltk.sent_tokenize(text):
     for chunk in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sent))):
         if hasattr(chunk, 'label'):
